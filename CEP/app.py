a93491bc0ba9133ec2f94e8a6f73fcb37ec5a1f6
ManthanMadankar <manthanmadankar9@gmail.com>
2026-03-31 13:49:46 +0530

    Update app run configuration to use environment port

--- CEP/app.py
+++ CEP/app.py
@@ -152,5 +152,7 @@ def predict():
         week_prediction=week_prediction
     )
 
+import os
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
